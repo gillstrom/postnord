69df59781effc6ed6faff2aea16a815d1bef16ca
Andreas Gillström <andreasgillstrom@gmail.com>
2016-04-14 20:27:06 +0200

    Tweak

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ import test from 'ava';
 import fn from './';
 
 test('postnord', async t => {
-	t.is(typeof await fn('69824790034SE', {apikey: 'bb956a2d13a54a152dba849b76288cf4'}), 'object');
+	t.is(typeof await fn('12324790034SE', {apikey: 'bb956a2d13a54a152dba849b76288cf4'}), 'object');
 });
